a8d7cdcd1d96697a26da720d2e5bac75851449e1
Shaun Saker <sakershaun@gmail.com>
2022-06-30 14:42:07 +0200

    update-frontend script

--- deploy/99-update-frontend.ts
+++ deploy/99-update-frontend.ts
@@ -1,20 +1,59 @@
 // TODO: I'm not such a big fan of this script, in production the FE will be a git repo
 // and this would change files in that repo. Ideally we would use a remote location where the abi could be fetched
-// (ideally directly from the contract on the blockchain?)
 
-import { ethers } from "hardhat"
+import { readFileSync, writeFileSync } from "fs"
+import { ethers, network } from "hardhat"
+import { join } from "path"
 
-const FRONT_END_ADDRESSES_FILE =
-  "../fcc-hardhat-smart-contract-lottery-frontend/constants/contractAddresses.json"
-const FRONT_END_ABI_FILE = "../fcc-hardhat-smart-contract-lottery-frontend/constants/abi.json"
+const FRONT_END_ADDRESSES_FILE = join(
+  __dirname,
+  "../../fcc-hardhat-smart-contract-lottery-frontend/constants/contractAddresses.json"
+)
+const FRONT_END_ABI_FILE = join(
+  __dirname,
+  "../../fcc-hardhat-smart-contract-lottery-frontend/constants/abi.json"
+)
+console.log(FRONT_END_ADDRESSES_FILE)
+console.log(FRONT_END_ABI_FILE)
 
 async function updateContractAddresses() {
   const raffle = await ethers.getContract("Raffle")
+  const chainId = network.config.chainId?.toString()
+
+  if (!chainId) {
+    console.error("No chainId!")
+
+    return
+  }
+
+  // TODO: this will fail if the file/parent folder does not exist
+  const currentAddresses = JSON.parse(readFileSync(FRONT_END_ADDRESSES_FILE, "utf-8"))
+
+  if (chainId in currentAddresses) {
+    if (!currentAddresses[chainId].includes(raffle.address)) {
+      currentAddresses[chainId].push(raffle.address)
+    }
+  } else {
+    currentAddresses[chainId] = [raffle.address]
+  }
+
+  writeFileSync(FRONT_END_ADDRESSES_FILE, JSON.stringify(currentAddresses))
+}
+
+async function updateAbi() {
+  const raffle = await ethers.getContract("Raffle")
+  const abi = raffle.interface.format(ethers.utils.FormatTypes.json)
+
+  writeFileSync(FRONT_END_ABI_FILE, abi)
 }
 
 module.exports = async function () {
   if (process.env.UPDATE_FRONTEND) {
     console.log("Updating front end...")
-    updateContractAddresses()
+    await updateContractAddresses()
+    await updateAbi()
+    console.log("Front end updated!")
   }
 }
+
+module.exports.tags = ["all", "frontend"]
